a8a6bdd697f3b07a93150cf41718e60af8872570
owenmaciel267 <owentiburon32@gmail.com>
2024-05-01 19:12:08 -0300

    configurar el crud para mostrar mascotas

--- mascotas.php
+++ mascotas.php
@@ -10,6 +10,8 @@
             $nuevoEmail = "";
             $nuevoMensaje = "";
 
+            
+
         // Crear (Insertar)
         if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["create"])) {
 
